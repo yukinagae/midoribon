b8cfa01e170bf4d5f144657ceacf1f9957a4b3ef
Yuki Nagae <yuki.nagae1130@gmail.com>
2018-01-27 01:10:30 +0900

    Add log likelihood function

---
+++ log_likelihood.R
@@ -0,0 +1,16 @@
+lambda <- 5.2
+
+log_likelihood <- vector(mode = "integer")
+
+for (i in 1:length(data)) {
+  a <- data[i]*log(lambda)
+  b <- lambda
+  c <- vector(mode = "numeric")
+  for (k in 1:data[i]) {
+    c[k] <- log(k)
+  }
+  log_likelihood[i] <- (a - b - sum(c))
+}
+
+sum(log_likelihood)
+
